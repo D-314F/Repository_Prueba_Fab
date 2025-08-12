22a0ff4562dc51299951c816abf3311e7fa30882
D-314F <fabianguzmanvillegas00@gmail.com>
2025-08-12 12:35:49 -0500

    Se agregaron operaciones en el java script pero falto el console.log

--- variables.js
+++ variables.js
@@ -1,5 +1,8 @@
 let numero1 = 45;
 let numero2 = 10;
-let resultado = numero1 + numero2;
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let division = numero1 / numero2;   
+let multiplicacion = numero1 * numero2;
 
 console.log(resultado);
